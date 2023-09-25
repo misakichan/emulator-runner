592497a0757c171489d9a3f6c44c6f1dcf7e3fbe
星冉 <Jalapeno1868@outlook.com>
2023-09-26 05:09:03 +0800

    页面初始化

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,15 +2,35 @@
 
 import {ipcRenderer} from "electron";
 import {t} from "../cli/i18n";
+import {openConfig, getConfig, saveConfig, reload} from "../cli/config";
+import {ref} from "vue";
+import path from "path";
+
+const {exec} = require('child_process');
+
+const config = ref(getConfig())
 
 function test1() {
-  console.log(t('dashboard'));
+  console.log(config.value.sdkPath);
+  exec(`${path.join(config.value.sdkPath, 'tools', 'bin', 'avdmanager')} -s list target`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`执行shell命令出错: ${err}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
 }
 
 </script>
 
 <template>
-  <bottom @click="test1"></bottom>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <bottom @click="test1">你好</bottom>
 </template>
 
 <style scoped>
